1670ca7800efe9938c407aceebcb49c70db12954
Itai Spigelman <itai10020@gmail.com>
2020-06-26 22:05:39 +0300

    Print HELLO from memory

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,17 +1,20 @@
 mov ah, 0x0e    ; scrolling teletype BIOS routine
 
-mov al, 'H'
-int 0x10
-mov al, 'E'
-int 0x10
-mov al, 'L'
-int 0x10
-mov al, 'L'
-int 0x10
-mov al, 'O'
-int 0x10
+mov cx, 0
+mov bx, hello
+add bx, 0x7c00
+print_hello:
+    mov al, [bx]
+    int 0x10
+    inc cx
+    inc bx
+    cmp cx, 5
+    jne print_hello
 
 jmp $
+
+hello:
+    db 'HELLO'
 
 times 510-($-$$) db 0
 
