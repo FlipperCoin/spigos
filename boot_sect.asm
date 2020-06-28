0c449363d7a06e8fa41ba097f520ba35dc67b20b
Itai Spigelman <itai10020@gmail.com>
2020-06-28 20:35:54 +0300

    Fix: stack init

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,10 +1,14 @@
 ;
-; Initialize data and stack segment
+; Init data segment
 ;
 mov bx, 0x7c0
 mov ds, bx
-mov bx, 0x800
-mov ss, bx
+
+;
+; Init stack
+;
+mov bp, 0x8000
+mov sp, bp
 
 ;
 ; Clear screen
@@ -16,6 +20,12 @@ int 0x10
 mov ax, HELLO
 call println
 mov dx, 0x12AB
+call print_hex
+push 0
+mov ax, sp
+call println
+push sp
+mov dx, sp
 call print_hex
 push 0
 mov ax, sp
