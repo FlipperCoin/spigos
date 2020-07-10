8d6c74877908a00cab8ce697ee1551ee766c1ed6
Itai Spigelman <itai10020@gmail.com>
2020-07-10 12:22:54 +0300

    WIP keyboard driver

--- drivers/src/keyboard.cpp
+++ drivers/src/keyboard.cpp
@@ -6,15 +6,57 @@
 #include <format.h>
 #include <types.h>
 
+#define NUMBER_OF_KEYS 256
+
+enum KeyEventType {
+    Pressed = 0,
+    Released = 1
+};
+
+enum KeyboardStateFlags {
+    CapsLock = 1,
+    ScrollLock = 2,
+    NumLock = 4
+};
+
+typedef struct KeyEvent {
+    uint_8 key;
+    KeyEventType type;
+    KeyboardStateFlags state;
+    KeyCode keymap[NUMBER_OF_KEYS];
+} KeyEvent;
+
+static uint_8 keysBuffer[KEYS_BUFFER_SIZE];
+static uint_32 keysIndex;
+
+static char shiftBufferLeft() {
+    char c = keysBuffer[0];
+    for (size_t i = 0; i < KEYS_BUFFER_SIZE - 1; i++)
+    {
+        keysBuffer[i] = keysBuffer[i+1];
+    }
+    keysIndex--;
+    return c;
+}
+
+static void appendKey(uint_8 key) {
+    if (keysIndex < KEYS_BUFFER_SIZE) {
+        keysBuffer[keysIndex++];
+    } else {
+        shiftBufferLeft();
+        keysBuffer[keysIndex++];
+    }
+}
+
+char readKey() {
+    return shiftBufferLeft();
+}
+
 __attribute__((interrupt))
 void 
 keyboardISR(interrupt_frame *frame) {
     uint_8 key = portByteIn(PS2_DATA_PORT);
-    
-    print("Key Event: ");
-    char buf[10];
-    itoh(key, buf, 10);
-    println(buf);
+    appendKey(key);
     
     sendEOI(false);
 }
