21392ca505917493834e2d77468752beb6f297bc
Itai Spigelman <itai10020@gmail.com>
2020-07-10 23:45:04 +0300

    clean print

--- kernel/print.cpp
+++ kernel/print.cpp
@@ -3,12 +3,9 @@ short* const VIDEO_MEM = (short*)0xb8000;
 void print(const char* s) {
     short whiteOnBlack = 0x0f00;
     short* videoBuffer = VIDEO_MEM;
-    for (; *s != '\0'; s++) {
-        short c = whiteOnBlack + (*s);
-        *videoBuffer = c;
+    while (char c = *(s++)) {
+        *videoBuffer = whiteOnBlack + c;
         videoBuffer++;
     }
-    // while (char c = *(s++) != '\0') {
-    //     *(videoBuffer++) = c;
-    // }
+    
 }
