21434fb8040d671cbe94b6cf9f105f5f1d62dbb6
Itai Spigelman <itai10020@gmail.com>
2020-07-11 03:38:16 +0300

    WIP keyboard driver

--- drivers/src/keyboard.cpp
+++ drivers/src/keyboard.cpp
@@ -5,8 +5,7 @@
 #include <screen.h>
 #include <format.h>
 #include <types.h>
-
-#define NUMBER_OF_KEYS 256
+#include <memory.h>
 
 enum KeyEventType {
     Pressed = 0,
@@ -19,15 +18,20 @@ enum KeyboardStateFlags {
     NumLock = 4
 };
 
+KeyboardStateFlags keyboardState;
+
 typedef struct KeyEvent {
-    uint_8 key;
+    KeyCode key;
     KeyEventType type;
     KeyboardStateFlags state;
-    KeyCode keymap[NUMBER_OF_KEYS];
+    bool pressedKeysKeymap[NUMBER_OF_KEYCODES];
 } KeyEvent;
 
 static uint_8 keysBuffer[KEYS_BUFFER_SIZE];
-static uint_32 keysIndex;
+static size_t keysIndex;
+
+static KeyEvent eventsBuffer[EVENTS_BUFFER_SIZE];
+static size_t eventsIndex;
 
 static char shiftBufferLeft() {
     char c = keysBuffer[0];
@@ -52,11 +56,62 @@ char readKey() {
     return shiftBufferLeft();
 }
 
+bool tryResolveSingleCharacterScanCode(uint_8 key, KeyCode *keycode, KeyEventType *type) {
+    // pressed
+    if (key >= 0x01 && key <= 0x58) { 
+        *type = KeyEventType::Pressed;
+    }
+    // released
+    else if (key >= 0x81 && key <= 0xD8) {
+        *type = KeyEventType::Released;
+        key -= 0x80;
+    }
+    else {
+        return false;
+    }
+
+    *keycode = (KeyCode)key;
+}
+
+bool tryResolveMultiCharacterScanCode(uint_8 key, KeyCode *keycode, KeyEventType *type) {
+
+}
+
+bool tryResolveScanCode(uint_8 key, KeyCode *keycode, KeyEventType *type) {
+    if (keysIndex == 0) {
+        if (key == 0x0E) { // Start of multi character scan code
+                keysBuffer[keysIndex++] = key;
+                return false;
+        }
+
+        return tryResolveSingleCharacterScanCode(key, keycode, type);
+    }
+    else // In middle of multi character scan code
+    {
+        return tryResolveMultiCharacterScanCode(key, keycode, type);
+    } 
+}
+
+void addKeyEvent(KeyCode keyCode, KeyEventType eventType) {
+    KeyEvent newEvent = {
+        keyCode,
+        eventType,
+        keyboardState,
+        eventsBuffer[eventsIndex-1].pressedKeysKeymap
+    };
+    newEvent.pressedKeysKeymap[keyCode] = eventType == KeyEventType::Pressed;
+    eventsBuffer[eventsIndex++] = newEvent;
+}
+
 __attribute__((interrupt))
 void 
 keyboardISR(interrupt_frame *frame) {
     uint_8 key = portByteIn(PS2_DATA_PORT);
-    appendKey(key);
+    KeyCode keyCode;
+    KeyEventType type;
+    if (tryResolveScanCode(key, &keyCode, &type)) {
+        addKeyEvent(keyCode, type);
+    }
     
     sendEOI(false);
 }
