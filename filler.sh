b1b21a91c045de4fde29657dea5759a01640da7a
Itai Spigelman <itai10020@gmail.com>
2020-07-19 17:20:23 +0300

    removed kernel.elf, filler not using calc

--- filler.sh
+++ filler.sh
@@ -2,5 +2,6 @@ if [ $# != 1 ]; then
     echo "Usage: $0 [OUTPUT_FILE]"
     exit 1
 fi
-
-truncate -s $(calc 512*51-$(calc $(ls -l kernel/out/bin/kernel.bin | awk '{ print $5 }')+$(ls -l boot/out/bin/boot_sect.bin | awk '{ print $5 }'))) $1
+BOOT_SECT_SIZE=$(ls -l boot/out/bin/boot_sect.bin | awk '{ print $5 }')
+KERNEL_SIZE=$(ls -l kernel/out/bin/kernel.bin | awk '{ print $5 }')
+truncate -s $((512*51-($BOOT_SECT_SIZE+$KERNEL_SIZE))) $1
